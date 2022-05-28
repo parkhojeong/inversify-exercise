e0fc9d5a379eaa0a876dabcaae005c07c1d0c33c
parkhojeong <parkhj062@gmail.com>
2022-05-28 23:18:05 +0900

    add inversify example code

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,8 @@
-import {TYPES} from "./types";
-import {ThrowableWeapon, Warrior, Weapon} from "./interfaces";
-import {inject, injectable} from "inversify";
-import "reflect-metadata"
+import { TYPES } from "./types";
+import { Warrior } from "./interfaces";
+import {myContainer} from "../inversify.config";
 
-@injectable()
-class Ninja implements Warrior {
-    @inject(TYPES.Weapon) private _katana: Weapon;
-    @inject(TYPES.ThrowableWeapon) private _shuriken: ThrowableWeapon;
-    public fight() { return this._katana.hit(); }
-    public sneak() { return this._shuriken.throw(); }
-}
+const ninja = myContainer.get<Warrior>(TYPES.Warrior);
 
-
-
+console.log(ninja.fight() === "cut!")
+console.log(ninja.sneak() === "hit!")
